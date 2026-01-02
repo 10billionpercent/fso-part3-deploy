b787282f30a0708756f7f74f1d54e9daf1cdb9f2
10billionpercent <103302612+10billionpercent@users.noreply.github.com>
2026-01-02 22:45:09 +0530

    Refactor mongo.js to accept command line arguments

--- mongo.js
+++ mongo.js
@@ -6,6 +6,8 @@ if (process.argv.length < 3) {
 }
 
 const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
 
 const url = `mongodb+srv://meowmeow:${password}@meow.aylzfms.mongodb.net/?appName=meow`
 mongoose.connect(url, { family: 4 })
@@ -18,11 +20,23 @@ const personSchema = new mongoose.Schema({
 const Person = mongoose.model('Person', personSchema)
 
 const person = new Person({
-name: 'meow',
-number: 4875639876,
+name: name,
+number: number,
 })
 
+if (process.argv.length > 3) {
 person.save().then(result => {
-    console.log('MEOW')
+    console.log(`Added ${name} number ${number} to phonebook`)
     mongoose.connection.close()
 })
+}
+
+if (process.argv.length === 3) {
+    Person.find({})
+    .then(persons => 
+        {console.log('phonebook')
+        persons.forEach(p => 
+        console.log(`${p.name} ${p.number}`))
+        mongoose.connection.close()
+        })
+}
